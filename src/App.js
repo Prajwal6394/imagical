b8a75b8011951f4425128f2845033b7e121b4f06
prajwal6394 <prajwalpandey2009@gmail.com>
2022-04-12 03:49:07 +0530

    making card using tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Imagical</h1>
+    <div className ="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl-mb-2">
+          Photo by Prajwal Pandey
+        </div>
+      </div>
     </div>
   );
 }
