84ba4debd4380a50b390124dc8d6b38b15744eb2
prajwal6394 <prajwalpandey2009@gmail.com>
2022-04-15 21:14:08 +0530

    grid completed

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -1,38 +1,33 @@
 import React from "react";
 
-const ImageCard = () => {
+const ImageCard = ({image}) => {
+  const tags = image.tags.split(",");
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <img src={image.webformatURL} alt="" className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl-mb-2">
-          Photo by Prajwal Pandey
+          Photo by {image.user}
         </div>
         <ul>
           <li>
             <strong>Views:</strong>
-            4000
+            {image.views}
           </li>
           <li>
             <strong>Downloads:</strong>
-            652
+            {image.downloads}
           </li>
           <li>
             <strong>Likes:</strong>
-            695
+            {image.likes}
           </li>
         </ul>
       </div>
       <div className="px-6-py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 ml-1 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
+        {tags.map((tag, index) => (<span key = {index}className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 ml-1 mr-2">
+          #{tag}
+        </span>))}
       </div>
     </div>
   );
